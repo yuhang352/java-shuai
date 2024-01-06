5c0db2a1df8c26c3175c77371ad508a9fcfb8c76
tang <tang@tang.com>
2024-01-06 17:29:19 +0800

    第二次提交

--- demo1/src/main/java/org/example/Main.java
+++ demo1/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        int bbbbbbb;
+        double bbbbbbbbbbbb;
+        System.out.println("第二次");
     }
 }
